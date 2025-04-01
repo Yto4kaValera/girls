744fe67ad275b730ea95701f27c527a64312ae33
counterattack01 <152067354+counterattack01@users.noreply.github.com>
2025-04-01 20:37:48 +0700

    Update server.py
    без заглушки

--- нейронка/server.py
+++ нейронка/server.py
@@ -1,6 +1,15 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 from flask import Flask, request, jsonify
 from PIL import Image
 import numpy as np
+from tensorflow.keras.models import load_model 
+from PIL import ImageOps
+
+
+#model = load_model("model.h5") #устаревшее
+model = load_model("model.keras")
 
 app = Flask(__name__)
 
@@ -10,7 +19,6 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    print("Получены файлы:", request.files) #проверка
 
     if "image" not in request.files: #проверка
         print("Ошибка: Файл не найден")
@@ -19,25 +27,45 @@ def predict():
     file = request.files["image"]
     
     try:
-        image = Image.open(file).convert("L")  #в ЧБ (на всякий)
-        print("Изображение открыто успешно")  #проверка
-
-        image = image.resize((50, 50))
-        print("Изображение изменено")  #проверка
-
-        image_array = np.array(image) / 255.0
-        print("Изображение преобразовано в массив")  #проверка
+        image = Image.open(file)
+        print("Картинка открыта успешно")
+        print("Размерность:", image.size)
 
 
-        #заглушка (то вместо чего должна выводить нейронка)
-        prediction = 88
-        print("Отправляю JSON:", {"prediction": prediction})  #лог
+        image = image.convert("L")
+        image = ImageOps.invert(image)
+        print("Картинка изменена")
 
-        return jsonify({"prediction": prediction})  #отправка угаданного числа
+
+        #image.show()  #проверка какая картинка стала
+
+
+        image_array = np.array(image, dtype=np.float32)     
+        print("Картинка преобразована в массив")
+
+
+        #image.show()  #тоже проверка
+
+        image_array = image_array/255.0
+        image_array = np.expand_dims(image_array, axis=0)  #batch размерность
+        image_array = np.expand_dims(image_array, axis=-1)  #канал (28x28x1)
+
+        print("Картинка подготовлена для нейронки")
+        
+        print("Форма входного массива в модель:", image_array.shape)    #должна быть 1 28 28 1
+        print("Вес слоя модели:", model.get_weights()[0].shape)         #проверка правильно ли подгружена модель
+        print("Мин. значение:", np.min(image_array), " Макс. значение:", np.max(image_array))   #вывод от 0 до 1
+
+
+        prediction = model.predict(image_array)  #предсказание
+        predicted_class = int(np.argmax(prediction)) 
+        print("Отправляю JSON:", {"prediction": predicted_class})  #лог
+
+        return jsonify({"prediction": predicted_class})  #отправка угаданного числа
     
     except Exception as e:  #перехват ошибок в блоке try
-        print("Ошибка при обработке изображения:", str(e))
-        return jsonify({"error": "Ошибка при обработке изображения"}), 500
+        print("Ошибка при обработке:", str(e))
+        return jsonify({"error": "Ошибка при обработке"}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
